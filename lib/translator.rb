b5c7bc95784dead95b66dd7a53c8cf7746b1e4b6
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-07 18:44:31 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,40 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(path)
+  file = YAML.load_file(path)
+  file.reduce({}) do |memo, (key, value)| 
+    memo[key] = {}
+    value.each_with_index {|e, i| i == 0 ? memo[key][:english] = e : memo[key][:japanese] = e }
+    memo
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  emoticons = load_library(path)
+  res = ""
+  emoticons.each {|pair| 
+    res = pair[1][:japanese] if pair[1][:english] == emoticon
+    break if res != ""
+  }
+  if res != ""
+    return res
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  emoticons = load_library(path)
+  res = ""
+  emoticons.each {|pair| 
+    res = pair[0] if pair[1][:japanese] == emoticon
+    break if res != ""
+  }
+  if res != ""
+    return res
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
