4cf59ef44b7ae73fdaef70744e3b93d35043b97f
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-07 18:06:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
+require 'yaml'
 
 def load_library
-  # code goes here
+  file = YAML.load_file('emoticons.yml')
+  file.reduce({}) do |memo, (key, value)| 
+    memo[key] = {}
+    value.each_with_index {|e, i| i == 0 ? memo[key][:english] = e : memo[key][:japanese] = e }
+    memo
+  end
 end
 
 def get_japanese_emoticon
@@ -11,3 +17,4 @@ end
 def get_english_meaning
   # code goes here
 end
+p load_library
